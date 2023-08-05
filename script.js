bf4089b9654e880e85a50e6043e1c2566671a838
DJsaru <smartysarangdj@gmail.com>
2023-08-05 13:51:17 +0530

    first

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 function loco(){
     gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
 });
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+
 locoScroll.on("scroll", ScrollTrigger.update);
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  },
   getBoundingClientRect() {
     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
   },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+
   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
 });
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+
 ScrollTrigger.refresh();
 
 }
@@ -83,10 +82,6 @@ var tl = gsap.timeline({
 tl.to("#page1>h1",{
     top:`-50%`
 })
-
-
-
-
 
 
 var tl1 = gsap.timeline({
@@ -515,9 +510,9 @@ ScrollTrigger.create({
 
 trigger: "#page18",
 pin: true,
-// markers:true,
+
 scroller: `#main`,
-//   set start end according to preference
+
 start: `top top`,
 end: `80% top`,
 });
